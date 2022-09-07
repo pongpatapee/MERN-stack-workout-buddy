0b257b30e63d1cc6acbfe048c7f17406c4e6cc1d
pongpatapee <pongpatapee1021@gmail.com>
2022-09-07 02:49:06 -0400

    added mongo schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,23 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({msg: 'GET all workouts'})
+  res.json({ msg: "GET all workouts" });
 });
 
 router.get("/:id", (req, res) => {
-    res.json({msg: 'GET single workout'})
+  res.json({ msg: "GET single workout" });
 });
 
 router.post("/", (req, res) => {
-    res.json({msg: 'POST a new workout'})
+  res.json({ msg: "POST a new workout" });
 });
 
 router.delete("/:id", (req, res) => {
-    res.json({msg: 'DELETE a new workout'})
+  res.json({ msg: "DELETE a new workout" });
 });
 
 router.patch("/:id", (req, res) => {
-    res.json({msg: 'UPDATE a new workout'})
+  res.json({ msg: "UPDATE a new workout" });
 });
 
 module.exports = router;
